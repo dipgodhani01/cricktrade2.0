358d7c997945a711b0753f48b6adad5e3bc6f705
dip godhani <dipgodhani1845@gmail.com>
2025-08-26 14:06:20 +0530

    initial update

--- app.js
+++ app.js
@@ -11,33 +11,21 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      
-      const allowedOrigins = [
-        "http://localhost:3011", 
-        "http://localhost:3000",
-        "http://localhost:5173",
-      ];
-      
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: ["http://localhost:3011", "http://localhost:3000","http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
+        allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
+
   })
 );
 app.options('*', cors());
+
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/admin",adminRouter)
 
 seedAdmin();
 connection();
